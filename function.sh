245cc756df6eef88b92b6c9c8fde48028d2499a1
DivineNN <ngantchadi@yahoo.com>
2023-09-19 08:39:11 -0400

    my first comit

---
+++ function.sh
@@ -0,0 +1,9 @@
+In today's session, we are going to see: 
+what are git and github
+what is an organisation in github
+what is a repository in github
+what is the working area in git
+what is a staging area in git
+what is a local repository in git
+what is a remote repository in git
+what is a commit 
